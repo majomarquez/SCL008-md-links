6756e83e5039f5ed3a9ccde08f20f8826f745bea
majomarquez <majomarquez@gmail.com>
2019-04-24 10:48:13 -0400

    lee links

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = () => {
-  // ...
-};
+
+const mdLink =require('./md-Links');
+const fs = require("fs");
+var markdownLinkExtractor = require('markdown-link-extractor');
+
+var markdown = fs.readFileSync('README.md').toString();
+
+var links = markdownLinkExtractor(markdown);
+
+links.forEach(function (link) {
+    console.log(link);
+});
+
+
---
+++ md-Links.js
@@ -0,0 +1,45 @@
+// // const mdLinks = require("md-links");
+// // const fs =requiere('fs');
+// const markdownLinkExtractor =require('markdownLinkExtractor')
+// const markdown = fs.readFileSync(command).toString();
+// const links=markdownLinkExtractor(markdown)
+ 
+
+
+
+// links.forEach(function(element){
+// console.log(res.url+ " "+ res.status + " "+ res.statusText);
+// })
+// .catch (error =>{
+//     console.log(error.message)
+// });
+    
+
+// exports.links=links;
+
+
+
+
+
+
+
+// mdLinks("../README.md")
+//   .then(links => {
+//     // => [{ href, text, file }]
+//   })
+//   .catch(console.error);
+
+// mdLinks("../README.md", { validate: true }) // si ademas me enviaron el parametro validate
+//   .then(links => {
+//     // => [{ href, text, file, status, ok }]
+//   })
+//   .catch(console.error);
+
+// mdLinks("../README.md") // aca le paso el directorio y busca los archivos .md 
+//   .then(links => {
+//     // => [{ href, text, file }]
+//   })
+//   .catch(console.error);
+
+
+
