abe7b63579c6292bce6d41c29b8d9e373a3b40c7
majomarquez <majomarquez@gmail.com>
2019-04-30 15:19:45 -0400

    contante ruta

--- md-Links.js
+++ md-Links.js
@@ -1,10 +1,63 @@
+#!/usr/bin/env 
+"use strict" // buscar tambien
+//fetch package 
+const fetch = require("node-fetch");
+const FileHound = require('filehound');
 
-    function linkStatus (){
-        return new Promise (function(resolve,reject){
-//aca escribo lo que necesito para mostrar el status
-         resolve("aca va la variable que contiene el status")  
+const fs = require("fs");
+var markdownLinkExtractor = require('markdown-link-extractor');
+// const path = require('path'); // pasra que me reconozca la ruta absoluta
 
-        });
-    }
+const ruta= process.argv[2]
+//filehound busca en la carpeta 
+const files = FileHound.create()
+  .paths(ruta)
+  .ext('md')
+  .find();
 
-//para el 
+// recorre los .md
+files.then(res=>{
+  res.forEach(function (fileAll) {
+        
+//markdown imprime toooooodoo los .md 
+fs.readFile(fileAll, (err, data) => {
+  if (err) throw err;
+  processMarkdown(data.toString());
+ 
+});  
+
+function processMarkdown(filecontent) {
+  //links lee los links dentro de los archivos
+  let links = markdownLinkExtractor(filecontent);
+  // let validLinks = 0;
+  // let invalidLinks = 0;
+
+if (links.length){
+  
+  links.forEach(function (link) { 
+    fetch(link)
+    .then(res => {
+      
+      if (res.ok) {  
+        console.log("This link is Working" +"  " +res.url );
+        // validLinks += 1;
+      }  
+    })
+    .catch(error => {
+      console.log("Error catched FIX ME! "+ error.message );
+      // validLinks -= 1;
+      // invalidLinks += 1;
+    });
+});
+
+}else{
+  console.log("There are no links");
+  }
+  // lee cada link de readme
+ 
+     
+  console.log("From total links " + links.length )//+ , valid are ' + validLinks + ' and invalid ' + invalidLinks
+}
+
+});
+});
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,16 @@
-const mdLinks = require('../');
+
+const link= require('./md-Links');
 
 
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
-
-});
+/*1) Validaciones de las funciones que chequean createAccount()*/
+// describe("processMarkdown",()=>{
+//     it('deberia retornar false, si',()=>{
+//         expect(processMarkdown("")).toBe(false);
+//     })
+// })
+/*1) Validaciones de las funciones link()*/
+describe("link",()=>{
+    it('deberia retornar true, si',()=>{
+        expect(link("https://github.com/jsdom/jsdom")).resolve.toBe(true);
+    })
+})
